0d8b3326e9141ffc39a084147406a828fa926da7
Adam Rubin <adam@richminds.com>
2013-08-28 00:15:33 -0400

    Putting stripe into production mode

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,4 @@
-Stripe.api_key = "sk_sB4Pq6zuMPutBqOuX0j7CNbzfkXUw"
-STRIPE_PUBLIC_KEY = "pk_sgMGdcpy0H8F1OqdH0J7esjxavF0O"
+Stripe.api_key = "sk_7iKnt57EtGQyEQvk7dYD03CtGSGfm"
+STRIPE_PUBLIC_KEY = "pk_neubDI84lQmB6lgnlJRD885GqtazV"
+# Stripe.api_key = "sk_sB4Pq6zuMPutBqOuX0j7CNbzfkXUw"
+# STRIPE_PUBLIC_KEY = "pk_sgMGdcpy0H8F1OqdH0J7esjxavF0O"
